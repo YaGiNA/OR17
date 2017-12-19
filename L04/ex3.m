3948f01ef1b9659dbf1f7a47a3c2c58a6f1db402
YaGiNA <yahyu10101@gmail.com>
2017-12-19 16:05:27 +0900

    WIP: ex3

--- L04/ex3.m
+++ L04/ex3.m
@@ -1,11 +1,16 @@
+run('/usr/local/class/object/MATLAB/vlfeat/vl_setup');
+addpath('/usr/local/class/object/MATLAB/sift');
 load('db_pos.mat');
 rand_pos_imgs=database_pos(1:100,:);
 
 load('db_neg.mat');
 sel=randperm(900,100);
 rand_neg_imgs=database_neg(sel,:);
-rand_neg_list=neg_list(sel);
-
+vec=[];
 for i = 1:100
+    I_pos = rand_pos_imgs(i);
+    [pnt_pos,desc_pos] = sift_rand(I_pos,'randn',300);
     
 end
+% [codebook,idx]=vl_kmeans(vec,500);
+% save('codebook.mat','codebook');
