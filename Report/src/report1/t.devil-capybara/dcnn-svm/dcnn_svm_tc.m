d49f8c3afe4de931e5f02a8252f5c4d87ea759d8
YaGiNA <yahyu10101@gmail.com>
2018-02-07 22:46:59 +0900

    Add sequence of outputting faild images

--- Report/src/report1/t.devil-capybara/dcnn-svm/dcnn_svm_tc.m
+++ Report/src/report1/t.devil-capybara/dcnn-svm/dcnn_svm_tc.m
@@ -16,6 +16,7 @@ data_neg = dcnn_list(:, 201:400);
 cv=5;
 idx=[1:200];
 accuracy=zeros(1, 5);
+result = zeros(1, 400);
 
 % idx番目(idxはcvで割った時の余りがi-1)が評価データ
 % それ以外は学習データ
@@ -42,8 +43,29 @@ for i=1:cv
   pcount=numel(find((plabel .* eval_label)==1));
   ncount=numel(find((plabel .* eval_label)==-1));
   accuracy(i) = pcount/(pcount+ncount);
+  for k = 1:numel(plabel)
+      % 個々の分類正誤を出力(正: 0 誤: 1)
+      if i == 1
+          index = k * 5;
+      else
+          index = (k-1) * 5 + i - 1;
+      end
+      result(index) = abs(plabel(k) - eval_label(k))/2;
+  end
+  
 end
 fprintf('accuracy: %f\n',mean(accuracy))
+% ポジ/ネガで誤りだった画像をそれぞれ5枚ずつ表示する
+load('filelist-tc.mat');
+res_pos = result(1:200);
+res_neg = [zeros(1, 200) result(201:400)];
+fails_pos = list([find(res_pos == 1)]);
+fails_neg = list([find(res_neg == 1)]);
+for i=1:2
+    subplot(2, 2, i); imshow(fails_pos{i});
+    subplot(2, 2, i+2); imshow(fails_neg{i});
+end
+
 %{
 前準備: listを対象にすべての画像のDCNN特徴量をもつリストを
 tic; mk_dcnnlist.m(list) toc;
